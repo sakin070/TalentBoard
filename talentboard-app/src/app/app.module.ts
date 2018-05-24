4ba0374e4159afbef2ef84ac5511848a229cbe72
Hamza Basrai <hamza.basrai@gmail.com>
2018-05-24 00:13:03 -0400

    Created applicant component

--- talentboard-app/src/app/app.module.ts
+++ talentboard-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ApplicantComponent } from './applicant/applicant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicantComponent
   ],
   imports: [
     BrowserModule
